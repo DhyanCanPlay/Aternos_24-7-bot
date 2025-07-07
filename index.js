9b944bd5d0adf643941dfc55058f5e47beb03603
DhyanCanplay <Dhyanpatoliya1@gmail.com>
2025-07-07 23:44:38 +0100

    all done for simple bot

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>hi</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
 
 const HOST = 'vamsikiduniya.aternos.me';
 const USERNAME = 'ChomuBot';
